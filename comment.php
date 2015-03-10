34e74f4cd682dd818598c83146a7b730fe485189
LevisVentus <LevisVentus@users.noreply.github.com>
2015-03-10 17:37:29 +0700

    Update comment.php

--- comment.php
+++ comment.php
@@ -11,7 +11,7 @@ if($Posting){
 		$masukin= mysql_query("INSERT INTO commentdata VALUES ('','$commentText')")
 		or die ("Something goes wrong");
 	}else echo "Comment too long";
-	}else echo "please insert comment";
+	}else echo "apa yg mau di insert?";
 };
 
 ?>
