f9d7c65b1e238102cfc2c2f0f08d7b960a96c3ff
yudhyovian <yudhistira.yovian@gmail.com>
2015-03-10 17:33:24 +0700

    Create dbcon.php

---
+++ dbcon.php
@@ -0,0 +1,19 @@
+<?php
+$mysql_hostname = "127.0.0.1";
+$mysql_username = "root";
+$mysql_password = "admin123";
+$mysql_database = "kppl_project";
+
+$dbcon = mysql_connect ($mysql_hostname, $mysql_username, $mysql_password)
+or die("Can't connect to the database");
+echo "Connection Succesfull<br>";
+
+mysql_select_db($mysql_database, $dbcon) 
+or die("Error:Database not Found!!");
+echo "Connection Complete";
+
+
+?>
+
+
+	
